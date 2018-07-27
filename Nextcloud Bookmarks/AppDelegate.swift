87675f0c34856311d06bd25430811874c5224661
Richard Klose <richard.klose@auerswald.de>
2018-07-27 13:45:07 +0200

    Initial Commit

---
+++ Nextcloud Bookmarks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Nextcloud Bookmarks
+//
+//  Created by Richard Klose on 27.07.18.
+//  Copyright © 2018 Richard Klose. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Nextcloud Bookmarks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nextcloud Bookmarks
+//
+//  Created by Richard Klose on 27.07.18.
+//  Copyright © 2018 Richard Klose. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
